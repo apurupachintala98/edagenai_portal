a935a63a3f8120ecdb9d68ed56862756d051a050
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-04-28 00:00:22 +0530

    push

--- src/pages/Metrics/index.tsx
+++ src/pages/Metrics/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+} from "@carbon/react";
+import { useNavigate } from "react-router-dom";
+import { HeaderContainer, MainContainer, PageContainer, PageTitle } from "../styled.components";
+import { useWindowDimensions } from "utils/hooks";
 
 function Metrics() {
+  const navigate = useNavigate();
+  const { height } = useWindowDimensions();
   return (
-    <div>
-      <div>Metrics</div>
-    </div>
-  )
+    <MainContainer height={height}>
+      <PageContainer>
+        <HeaderContainer>
+          <PageTitle>Metrics</PageTitle>
+        </HeaderContainer>
+
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <div style={{ cursor: "pointer" }} onClick={() => navigate("/home")}>
+              Home
+            </div>
+          </BreadcrumbItem>
+          <BreadcrumbItem isCurrentPage>Metrics</BreadcrumbItem>
+        </Breadcrumb>
+        <div style={{ display: "flex", height: "70vh", alignItems: "center", justifyContent: "center", flexDirection: "column", textAlign: "center" }}>
+          <h1 style={{ fontSize: "28px", fontWeight: "bold" }}>Coming Soon!!</h1>
+          <p style={{ fontSize: "16px", color: "#6b7280", marginTop: "12px" }}>
+            Exciting updates are on the way. Please check back later.
+          </p>
+        </div>
+      </PageContainer>
+    </MainContainer>
+  );
 }
 
-export default Metrics
+export default Metrics;
