f628469e7afb8b645ac86974cd24c089d3cdf3bc
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-04-27 23:52:32 +0530

    push

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -616,7 +616,7 @@ function Project() {
           </BreadcrumbItem>
           <BreadcrumbItem isCurrentPage>Project</BreadcrumbItem>
         </Breadcrumb>
-        <TableContainer style={{ marginTop: "20px" }}>
+        <TableContainer style={{ marginTop: "20px", height: "100%" }}>
           {loading ? (
             <div style={{ padding: "20px", textAlign: "center" }}>
               Loading Projects...</div>
