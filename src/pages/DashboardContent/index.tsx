7a172d226de574b60abeeb6807f18b5f6d86a0e3
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-07-21 11:42:41 +0530

    push

--- src/pages/DashboardContent/index.tsx
+++ src/pages/DashboardContent/index.tsx
@@ -412,6 +412,7 @@ function DashboardContent({ containerWidth }: DashboardContentProps) {
               <Button
                 size="md"
                 renderIcon={PresentationFile}
+                hasIconOnly
                 onClick={() => {
                   window.open("https://link-to-frameworks-presentation.com", "_blank");
                 }}
