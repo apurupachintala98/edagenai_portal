7aa975be074ed188c45d57c2a08415f140e083a5
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-07-21 15:23:07 +0530

    push

--- src/hooks/DownloadDashboardPPT.tsx
+++ src/hooks/DownloadDashboardPPT.tsx
@@ -45,20 +45,20 @@ const downloadDashboardPPT = (charts: ChartImages[]) => {
   const pptx = new PptxGenJS();
   const slide = pptx.addSlide();
 
-//   slide.addText("Project Dashboard Charts", {
-//     x: 0.2,
-//     y: 0.2,
-//     fontSize: 18,
-//     bold: true,
-//     color: "363636",
-//   });
+  slide.addText("Dashboard Charts", {
+    x: 0.2,
+    y: 0.2,
+    fontSize: 18,
+    bold: true,
+    color: "363636",
+  });
 
   const width = 3.0;
   const height = 2.0;
 
   const positions = [
     [0.2, 1], [3.4, 1], [6.8, 1],
-    [0.2, 3.3], [3.4, 3.3], [6.8, 3.3],
+    [0.2, 3.5], [3.4, 3.5], [6.8, 3.5],
   ];
 
   charts.slice(0, 6).forEach((chart, idx) => {
